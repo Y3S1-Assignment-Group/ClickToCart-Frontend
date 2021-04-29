b442d81ea8c5492177190291b0d7c296bdbb6290
SenuraJayadeva <senurajayadeva@gmail.com>
2021-04-29 16:52:30 +0530

    implemeted Card Payment Gateway

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "../components/common/Navbar/Navbar";
 import Footer from "../components/common/Footer/Footer";
-import HomeHeroCarousel from "../modules/HomePageModules/HomeHeroSection/HomeHeroCarousel";
 import ExploreShoppingCart from "../modules/ExplorePageModules/ExploreShoppingCart/ExploreShoppingCart";
 class ExplorePage extends Component {
   render() {
