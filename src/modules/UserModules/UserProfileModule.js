229ad98dd7f1cea365f669b1a4a36b9ef15d14b0
ayodyabanuka <ayodyabanukafernando@gmail.com>
2021-05-02 16:45:51 +0530

    update

--- src/modules/UserModules/UserProfileModule.js
+++ src/modules/UserModules/UserProfileModule.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import user from '../../assets/img/UserProfilePage/user.png';
-import Payment from "../PaymentModules/Payment";
-import * as actions from "../../actions/CartActions";
-
-
+import Payment from '../PaymentModules/Payment';
+import * as actions from '../../actions/CartActions';
 
 class UserProfileModule extends Component {
-
   constructor(props) {
     super(props);
     this.state = {};
@@ -17,25 +14,23 @@ class UserProfileModule extends Component {
     this.props.getPayedItemsFromCart(this.props.user.id);
   }
 
-
   render() {
     return (
-
       <div className='container mt-4'>
-        <h2><center>My Profile</center></h2>
-        <div className="row p-5">
-          <div className="col-lg-4 col-md-4 col-sm-12">
+        <h2>
+          <center>My Profile</center>
+        </h2>
+        <div className='row p-5'>
+          <div className='col-lg-4 col-md-4 col-sm-12'>
             <img
-              src="https://kittyinpink.co.uk/wp-content/uploads/2016/12/facebook-default-photo-male_1-1.jpg"
+              src='https://kittyinpink.co.uk/wp-content/uploads/2016/12/facebook-default-photo-male_1-1.jpg'
               alt='logo'
               style={{ width: '150px', height: '150px' }}
             />
           </div>
-          <div className="col-lg-8 col-md-8 col-sm-12 mt-5">
+          <div className='col-lg-8 col-md-8 col-sm-12 mt-5'>
             <div className='row mb-2'>
-              <div className='col-lg-4 col-md-4 col-sm-6 text-muted '>
-                Name
-                    </div>
+              <div className='col-lg-4 col-md-4 col-sm-6 text-muted '>Name</div>
               <div className='col-lg-8 col-md-8 col-sm-6 '>
                 {this.props.user.username}
               </div>
@@ -43,24 +38,25 @@ class UserProfileModule extends Component {
             <div className='row mb-2'>
               <div className='col-lg-4 col-md-4 col-sm-6 text-muted '>
                 Email
-                    </div>
+              </div>
               <div className='col-lg-8 col-md-8 col-sm-6 '>
                 {this.props.user.email}
               </div>
             </div>
           </div>
         </div>
-        <div class="card bg-warning">
-          <div class="card-body mt-2">
+        <div class='card bg-warning'>
+          <div class='card-body mt-2'>
             <div className='row mb-2'>
-              <table className="table table-md table-responsive">
-                <div className="table-responsive">
-                  <table className="table align-middle">
+              <table className='table table-md table-responsive'>
+                <div className='table-responsive'>
+                  <table className='table align-middle'>
                     <thead>
-                      <th scope="col">Product</th>
-                      <th scope="col">Quantity</th>
-                      <th scope="col">Price</th>
-                      <th scope="col"></th>
+                      <th scope='col'>Product</th>
+                      <th scope='col'>Quantity</th>
+                      <th scope='col'>Price</th>
+                      <th scope='col'>Purchased Date</th>
+                      <th scope='col'></th>
                     </thead>
                     <tbody>
                       {this.props.payedCartList.map((cartItem) => {
@@ -69,6 +65,7 @@ class UserProfileModule extends Component {
                             <td>{cartItem.itemName}</td>
                             <td>{cartItem.qty}</td>
                             <td>{cartItem.totalPrice}</td>
+                            <td>{cartItem.modifiedDate}</td>
                           </tr>
                         );
                       })}
@@ -76,17 +73,17 @@ class UserProfileModule extends Component {
                       <tr>
                         <td></td>
                         <td>
-                          <h5 className="font-weight-bold">Total</h5>
+                          <h5 className='font-weight-bold'>Total</h5>
                         </td>
                         <td>
-                          <h5 className="font-weight-bold">
+                          <h5 className='font-weight-bold'>
                             {this.props.payedCartList.reduce(
                               (accumulator, current) =>
                                 accumulator + current.totalPrice,
                               0
                             )}
-                        USD
-                      </h5>
+                            USD
+                          </h5>
                         </td>
                         <td></td>
                       </tr>
